abc27ebc72f1fe2178063f1c5b3ee05e555a1b73
Maxime Bouveron <maxime.bouveron@gmail.com>
2017-10-06 09:04:38 +0200

    ➕ Added styled-components

--- src/App.js
+++ src/App.js
@@ -1,19 +1,41 @@
 import React, { Component } from 'react'
+import styled, { keyframes } from 'styled-components'
 import logo from './logo.svg'
-import './App.css'
+
+const appLogoSpin = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+`
+const AppLogo = styled.img`
+  height: 80px;
+  animation: ${appLogoSpin} infinite 20s linear;
+`
+const AppHeader = styled.header`
+  background-color: #222;
+  height: 150px;
+  padding: 20px;
+  color: white;
+`
+const AppWrap = styled.div`text-align: center;`
+const AppTitle = styled.h1`font-size: 1.5em;`
+const AppIntro = styled.p`font-size: large;`
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
+      <AppWrap>
+        <AppHeader>
+          <AppLogo src={logo} alt="logo" />
+          <AppTitle>Welcome to React</AppTitle>
+        </AppHeader>
+        <AppIntro>
           To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+        </AppIntro>
+      </AppWrap>
     )
   }
 }
