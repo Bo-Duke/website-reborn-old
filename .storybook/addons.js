23c25dd637da7463067262470f473155b8246a66
Maxime Bouveron <maxime.bouveron@gmail.com>
2018-01-30 16:53:16 +0100

    :heavy_minus_sign: Removing storybook (totally useless)

--- .storybook/addons.js
+++
@@ -1,2 +0,0 @@
-import '@storybook/addon-backgrounds/register'
-// import 'storybook-styled-components/register'
--- .storybook/config.js
+++
@@ -1,7 +0,0 @@
-import { configure, addDecorator } from '@storybook/react'
-
-function loadStories() {
-  require('../stories')
-}
-
-configure(loadStories, module)
