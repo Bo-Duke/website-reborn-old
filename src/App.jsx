a6c7d868dc60c0b65db8416355d1c775425e1743
Maxime Bouveron <maxime.bouveron@gmail.com>
2018-01-31 13:18:51 +0100

    :wrench: Made eslint work

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import styled from 'styled-components'
+
+const HelloWrap = styled.div`
+  text-align: center;
+`
+
+class App extends Component {
+  render() {
+    return <HelloWrap>Hello</HelloWrap>
+  }
+}
+
+export default App
