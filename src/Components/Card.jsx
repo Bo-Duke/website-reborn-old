ef9ffedd0f05732ad65def0f0ea11acf2a338ac3
Maxime Bouveron <maxime.bouveron@gmail.com>
2018-09-06 11:11:25 +0200

    :lipstick: Added parallax!

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -9,7 +9,7 @@ const StyledCard = styled.div`
   justify-content: center;
   max-width: 90%;
   padding: 15px;
-  border-radius: 2px;
+  border-radius: 10px;
   box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
   transition: box-shadow 0.3s cubic-bezier(0.25, 0.8, 0.25, 1);
   &:hover {
--- src/Containers/App.jsx
+++ src/Containers/App.jsx
@@ -1,30 +1,99 @@
-import React, { Component } from 'react'
-import styled from 'styled-components'
+import React, { Component, Fragment } from 'react'
+import { Parallax } from 'react-spring'
 import Card from '../Components/Card'
 import MarkdownRenderer from '../Components/MarkdownRenderer'
-import Banner from '../Components/Banner'
 import data from '../data/data.fr.yml'
 
-const Wrapper = styled.div`
-  background-color: #dddddd;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  flex-direction: column;
-
-  padding-top: 15px;
-`
-
+const BackgroundGradients = () => (
+  <Fragment>
+    <Parallax.Layer
+      id="page-1"
+      offset={0}
+      speed={0}
+      style={{ background: 'linear-gradient(black, Indigo)' }}
+    />
+    <Parallax.Layer
+      id="page-2"
+      offset={1}
+      speed={0}
+      style={{ background: 'linear-gradient(Indigo, DeepSkyBlue)' }}
+    />
+    <Parallax.Layer
+      id="page-3"
+      offset={2}
+      speed={0}
+      style={{ background: 'linear-gradient(DeepSkyBlue, yellow)' }}
+    />
+    <Parallax.Layer
+      id="page-4"
+      offset={3}
+      speed={0}
+      style={{ background: 'linear-gradient(yellow, ForestGreen)' }}
+    />
+    <Parallax.Layer
+      id="page-5"
+      offset={4}
+      speed={0}
+      style={{ background: 'linear-gradient(ForestGreen, SaddleBrown)' }}
+    />
+  </Fragment>
+)
 class App extends Component {
   render() {
+    const style = {
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+    }
     return (
-      <Wrapper>
-        <Banner />
-        <Card>
-          <h1>{data.name}</h1>
-          <p>{data.bio.long}</p>
-        </Card>
-      </Wrapper>
+      <Parallax ref={ref => (this.parallax = ref)} pages={5}>
+        <BackgroundGradients />
+        <Parallax.Layer offset={0} speed={0.5} style={style}>
+          <Card>
+            <h1>{data.name}</h1>
+            <p>{data.bio.long}</p>
+          </Card>
+        </Parallax.Layer>
+        <Parallax.Layer offset={1} speed={0.5} style={style}>
+          <Card>
+            <h1>Skills</h1>
+            <ul>
+              {data.skills.map(skill => (
+                <li key={skill['fa-name']}>{skill.name}</li>
+              ))}
+            </ul>
+          </Card>
+        </Parallax.Layer>
+        <Parallax.Layer offset={2} speed={0.5} style={style}>
+          <Card>
+            <h1>Work</h1>
+            <ul>
+              {data.jobs.map(job => (
+                <li>
+                  {job.name} - {job.company}
+                </li>
+              ))}
+            </ul>
+            <h1>School</h1>
+            <ul>
+              {data.formations.map(formation => (
+                <li>
+                  {formation.name} - {formation.localisation}
+                </li>
+              ))}
+            </ul>
+          </Card>
+        </Parallax.Layer>
+        <Parallax.Layer offset={3} speed={0.5} style={style}>
+          <Card>Projects</Card>
+        </Parallax.Layer>
+        <Parallax.Layer offset={4} speed={0.5} style={style}>
+          <Card>
+            <h1>About me</h1>
+            <MarkdownRenderer source={data.aboutme} />
+          </Card>
+        </Parallax.Layer>
+      </Parallax>
     )
   }
 }
